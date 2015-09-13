68ac89afafb4f3fe38252947f184567e0e1196c0
kennedy <kennedysgarage@gmail.com>
2015-09-13 13:07:13 -0400

    removed *connect* from the url

--- event_meet/urls.py
+++ event_meet/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [url(r'^connect/', include('circly.urls', namespace="connect")),
+urlpatterns = [url(r'', include('circly.urls', namespace="connect")),
                url(r'^admin/', include(admin.site.urls)),
 ]
