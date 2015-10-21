ac7c3a9a99c8046dbe4be7b3a310c94660e44519
mik3cap <mik3cap@gmail.com>
2015-10-20 22:48:56 -0400

    Change in models.py

--- circly/models.py
+++ circly/models.py
@@ -5,31 +5,31 @@ from django.db.models import URLField, SmallIntegerField, UUIDField
 from django.utils import timezone
 
 
-class Event(models.Model):
-    event_id = models.CharField(max_length=50)
-    event_name = models.CharField(max_length=200)
-    event_date = models.DateTimeField('date of event')
+class Event(Model):
+    event_id = CharField(max_length=50)
+    event_name = CharField(max_length=200)
+    event_date = DateTimeField('date of event')
 
     def __unicode__(self):
         return self.event_name
 
 
-class Attendee(models.Model):
-    event = models.ForeignKey(Event)
-    attendee_id = models.CharField(max_length=50)
-    attendee_email = models.CharField(max_length=200)
-    attendee_name = models.CharField(max_length=100)
-    checked_in = models.BooleanField(default=False)
+class Attendee(Model):
+    event = ForeignKey(Event)
+    attendee_id = CharField(max_length=50)
+    attendee_email = CharField(max_length=200)
+    attendee_name = CharField(max_length=100)
+    checked_in = BooleanField(default=False)
 
     def __unicode__(self):
         return self.attendee_name
 
 
-class Circle(models.Model):
-    circle_name = models.CharField(max_length=100)
-    circle_created_date = models.DateTimeField('circle created on')
-    circle_reminders_refreshed_on_date = models.DateTimeField('reminders refreshed on', null=True, blank=True)
-    circle_short_url = models.URLField(null=True, blank=True)
+class Circle(Model):
+    circle_name = CharField(max_length=100)
+    circle_created_date = DateTimeField('circle created on')
+    circle_reminders_refreshed_on_date = DateTimeField('reminders refreshed on', null=True, blank=True)
+    circle_short_url = URLField(null=True, blank=True)
 
     def circle_owner_name(self):
         circle_owner = self.member_set.filter(circle_owner=True)
@@ -42,39 +42,39 @@ class Circle(models.Model):
         return self.circle_name
 
 
-class Member(models.Model):
+class Member(Model):
     from choices_member import *
 
-    circle = models.ForeignKey(Circle)
-    circle_owner = models.BooleanField(default=False)
-    member_name = models.CharField(max_length=100)
-    member_created_date = models.DateTimeField('member created on', null=True, blank=True)
-    member_email = models.CharField(max_length=200, null=True, blank=True)
-    member_phone = models.CharField(max_length=25, null=True, blank=True)
+    circle = ForeignKey(Circle)
+    circle_owner = BooleanField(default=False)
+    member_name = CharField(max_length=100)
+    member_created_date = DateTimeField('member created on', null=True, blank=True)
+    member_email = CharField(max_length=200, null=True, blank=True)
+    member_phone = CharField(max_length=25, null=True, blank=True)
 
-    age_range = models.CharField(max_length=5,
-                                 choices=AGE_RANGE_CHOICES,
-                                 default=BTWN_20_29)
+    age_range = CharField(max_length=5,
+                          choices=AGE_RANGE_CHOICES,
+                          default=BTWN_20_29)
 
-    sex_range = models.CharField(max_length=2,
-                                 choices=SEX_CHOICES,
-                                 default=FEMALE)
+    sex_range = CharField(max_length=2,
+                          choices=SEX_CHOICES,
+                          default=FEMALE)
 
-    ethnicity_range = models.CharField(max_length=3,
-                                       choices=ETHNICITY_CHOICES,
-                                       default=WHITE)
+    ethnicity_range = CharField(max_length=3,
+                                choices=ETHNICITY_CHOICES,
+                                default=WHITE)
 
-    bmi_range = models.CharField(max_length=3,
-                                 choices=BMI_CHOICES,
-                                 default=BMI_UNDER)
+    bmi_range = CharField(max_length=3,
+                          choices=BMI_CHOICES,
+                          default=BMI_UNDER)
 
-    smoker = models.BooleanField(default=False)
-    drinker = models.BooleanField(default=False)
-    exercises = models.BooleanField(default=True)
+    smoker = BooleanField(default=False)
+    drinker = BooleanField(default=False)
+    exercises = BooleanField(default=True)
 
-    cancer_family = models.SmallIntegerField(default=0)
+    cancer_family = SmallIntegerField(default=0)
 
-    member_profile_entered_date = models.DateTimeField('profile entered on', null=True, blank=True)
+    member_profile_entered_date = DateTimeField('profile entered on', null=True, blank=True)
 
 
     def risk(self):
@@ -100,30 +100,30 @@ class Member(models.Model):
         return self.member_name
 
 
-class Invitation(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    member = models.ForeignKey(Member)
-    invite_code = models.CharField(max_length=300, null=True, blank=True)
-    invite_short_url = models.URLField(null=True, blank=True)
-    invite_created_date = models.DateTimeField('invite created on')
-    invite_send_date = models.DateTimeField('send invite on', null=True, blank=True)
-    invite_sent_on_date = models.DateTimeField('invite sent on', null=True, blank=True)
-    member_joined_on_date = models.DateTimeField('member joined on', null=True, blank=True)
+class Invitation(Model):
+    id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    member = ForeignKey(Member)
+    invite_code = CharField(max_length=300, null=True, blank=True)
+    invite_short_url = URLField(null=True, blank=True)
+    invite_created_date = DateTimeField('invite created on')
+    invite_send_date = DateTimeField('send invite on', null=True, blank=True)
+    invite_sent_on_date = DateTimeField('invite sent on', null=True, blank=True)
+    member_joined_on_date = DateTimeField('member joined on', null=True, blank=True)
 
 
     def __unicode__(self):
         return unicode(self.id)
 
 
-class Reminder(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    member = models.ForeignKey(Member)
-    reminder_subject = models.CharField(max_length=65)
-    reminder_message = models.CharField(max_length=150)
-    reminder_created_date = models.DateTimeField('reminder created on')
-    reminder_send_date = models.DateTimeField('send reminder on', null=True, blank=True)
-    reminder_sent_on_date = models.DateTimeField('reminder sent on', null=True, blank=True)
-    member_reminded_on_date = models.DateTimeField('member reminded on', null=True, blank=True)
+class Reminder(Model):
+    id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    member = ForeignKey(Member)
+    reminder_subject = CharField(max_length=65)
+    reminder_message = CharField(max_length=150)
+    reminder_created_date = DateTimeField('reminder created on')
+    reminder_send_date = DateTimeField('send reminder on', null=True, blank=True)
+    reminder_sent_on_date = DateTimeField('reminder sent on', null=True, blank=True)
+    member_reminded_on_date = DateTimeField('member reminded on', null=True, blank=True)
 
 
     def is_an_email(self):
